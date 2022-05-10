1fd2670c980529c818fb707736bbc3c051fa509b
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-10 00:21:41 -0500

    fixed bug

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server // and force to true means that This allows the table to be overwritten and re-created 
 //if there is any changes
