d8aa9300481781e20005d291fd64b508d2c53096
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-10 15:55:52 -0500

    Added the html for the login page

--- controllers/htmlroutes/html-routes.js
+++ controllers/htmlroutes/html-routes.js
@@ -19,4 +19,22 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/he', (req, res) => {
+
+  User.findAll({
+    // exclude password
+    attributes: { exclude: ['password'] }
+  })
+  // then send the data to the user as json 
+  .then(dbPostData => {
+    console.log(dbPostData[0]);
+    // pass a single post object into the homepage template
+    res.render('login');
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 module.exports = router;
