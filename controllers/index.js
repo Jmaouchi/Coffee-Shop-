55e7df5efa38ca4efa1b0ed04928a4fd913e65b0
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-10 00:20:38 -0500

    added routes to get the html displayed

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 //get all the routes from the api folder 
 const apiRoutes = require('./api');
-
+const htmlRoutes = require('./htmlroutes');
 
 router.use('/api', apiRoutes);
-
+router.use('/html', htmlRoutes);
 
 
 router.use((req, res) => {
