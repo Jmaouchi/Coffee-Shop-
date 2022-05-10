ffeed2412a3edd25047bb63c6d37fb7edb549b57
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-09 22:04:55 -0500

    createad the server and the connection  the database

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const routes = require('./controllers');
+const routes = require('./controllers')
+const sequelize = require('./config/connetcion');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+// middlewares 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up express handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// app.use(routes);
+
+// turn on connection to db and server // and force to true means that This allows the table to be overwritten and re-created 
+//if there is any changes
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
