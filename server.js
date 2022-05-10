1578f74be26310690261cbc9821a52d3575830a9
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-10 00:30:42 -0500

    added session and connect-session-sequelize packages

--- server.js
+++ server.js
@@ -18,6 +18,26 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+
+// set code to use express-session and sequelize.store
+// This code sets up an Express.js session and connects the session to our Sequelize database
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: ,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+// use sessions 
+app.use(session(sess));
+
+// use routes
 app.use(routes);
 
 // turn on connection to db and server // and force to true means that This allows the table to be overwritten and re-created 
