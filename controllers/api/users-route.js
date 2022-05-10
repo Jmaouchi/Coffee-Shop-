be011f1e58990bafdc367bc5d76fc4c09ec46ec3
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-10 00:14:56 -0500

    added routes to post, delete and update data in the user model

--- controllers/api/users-route.js
+++ controllers/api/users-route.js
@@ -72,5 +72,71 @@ router.get('/:id', (req, res) => {
 
 
 
+// post data 
+router.post('/', (req, res) => {
+  // in a post its always a create method that we need to use 
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+// delete data from the user table 
+router.delete('/:id', (req, res) => {
+  // in a post its always a create method that we need to use 
+  User.destroy({
+    
+    where:{id: req.params.id}
+
+  })
+    .then(dbUserData => {
+      if(!dbUserData){
+      res.status(404).json({message:'No user found with this id'});
+      return;
+    }
+    res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+
+// update data  in the user table 
+router.put('/:id', (req, res) => {
+
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, { 
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+
+
+
+
 
 module.exports = router;
