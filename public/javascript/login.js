6bc12600d1ee35f0e2cfd01f6fc6f012be0f3037
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-11 14:37:34 -0500

    fixed bug

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -51,7 +51,7 @@ async function signupFormHandler(event) {
 
     if (response.ok) {
       // after the account is been created, reffer them to another page 
-      document.location.replace('/login')
+      document.location.replace('/html/login')
     } else {
       alert(response.statusText);
     }
