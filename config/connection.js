2ba91951278919368311d57516602a2b26728fab
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-16 19:23:12 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,37 @@
-// // create a connection to database
-// const Sequelize = require('sequelize');
-
-// // use dotenv to hide the database name,  username and password 
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   });
-// }
-
-// // export sequelize
-// module.exports = sequelize;
-
-// Import the sequelize constructor
+// create a connection to database
 const Sequelize = require('sequelize');
 
+// use dotenv to hide the database name,  username and password 
 require('dotenv').config();
 
-// create connection to the db on Heroku
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+// export sequelize
 module.exports = sequelize;
+
+// Import the sequelize constructor
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+// // create connection to the db on Heroku
+// const sequelize = process.env.JAWSDB_URL
+//     ? new Sequelize(process.env.JAWSDB_URL)
+//     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+
+// module.exports = sequelize;
 
