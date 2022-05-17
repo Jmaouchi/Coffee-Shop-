e2b629e4193836a1c8c453ddb7d977b193e8d84c
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-16 21:33:26 -0500

    added login route

--- controllers/login-route.js
+++ controllers/login-route.js
@@ -4,7 +4,7 @@ const { User } = require('../models');
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/html');
+    res.redirect('/login');
     return;
   }
 
