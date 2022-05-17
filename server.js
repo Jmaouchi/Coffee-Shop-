8ac6b202feab53498a38134ed013b8d97ccbc484
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-16 19:49:12 -0500

    change url handler

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 // middlewares 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set up express handlebars
