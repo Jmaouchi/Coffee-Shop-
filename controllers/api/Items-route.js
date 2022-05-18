955dd763f2530cebc5fc86a0005eb69576dc33a7
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-18 14:44:03 -0500

    Added item routes back to api/routes

--- controllers/api/Items-route.js
+++ controllers/api/Items-route.js
@@ -1,11 +1,45 @@
-// Add an item
-
-const res = require("express/lib/response");
-const router = require(".");
-const Items = require("../../models/Items");
+const router = require('express').Router();
+const { User, Items } = require("../../models/");
 
 
+// Get all items
+router.get('/', async, (req, res) => {
+    post.findAll({
 
+        attributes: ['id', 'item_name', 'item_price', 'item_stock']
+
+    })
+    .then(dbItemData => res.json(dbItemData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Get one item by its ID
+router.get('/:id', (req, res) => {
+    post.findOne({
+
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'item_name', 'item_price', 'item_stock']
+    })
+    .then(dbItemData => {
+        if (!dbItemData) {
+            res.status(404).json({ message: 'No item exists with that ID, try again.' });
+            return;
+        }
+        res.json(dbItemData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// Add an item to the store
 router.post('./Items', (req, res) => {
 
     try {
@@ -20,3 +54,5 @@ router.post('./Items', (req, res) => {
         res.status(400).send({ message: 'error' });
     }
 });
+
+module.exports = router;
