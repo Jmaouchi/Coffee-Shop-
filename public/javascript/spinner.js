f6dea048b9deaf6ddcbd61edf8d56c3a3a516187
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-17 19:26:33 -0500

    added fixed bug

--- public/javascript/spinner.js
+++ public/javascript/spinner.js
@@ -1,6 +1,10 @@
+const myTimeout = setInterval(myGreeting, 50000);
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
 
+function myGreeting() {
+  window.addEventListener('load', function(){
+    spinnerWrapper.style.display = 'none'
+  })
+}
 
-window.addEventListener('load', function(){
-  spinnerWrapper.style.display = 'none'
-})
+myGreeting();
