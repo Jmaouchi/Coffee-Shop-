3a848656717ad17000bd383501c1bf7e8baa111b
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-18 14:49:54 -0500

    Added Items associations to user and reviews

--- models/index.js
+++ models/index.js
@@ -1,11 +1,27 @@
 const User = require('./Users');
 const Review = require('./Reviews');
+const Items = require('./Items');
+
+
 // create associations
 User.hasMany(Review, {
-  foreignKey: 'user_id'
+    foreignKey: 'user_id'
 });
 Review.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
-module.exports = { User, Review };
+
+User.hasMany(Items, {
+    foreignKey: 'user_id'
+});
+
+Items.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Item.hasMany(Review, {
+    foreignKey: 'item.id'
+});
+
+module.exports = { User, Review, Items };
