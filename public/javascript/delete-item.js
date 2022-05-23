786e254ecae80f78a2169f9495550f5572eb8da3
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-23 15:41:26 -0500

    added a delete function to the items section

---
+++ public/javascript/delete-item.js
@@ -0,0 +1,22 @@
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+  let attr = document.querySelector('.fa-times').getAttribute('data-x');
+  // console.log(attr);
+  if (event.target.matches('.fa-times')) {
+    event.target.parentElement.remove();
+  }
+
+  const id = attr;
+  const response = await fetch(`/api/items/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.cart-items-container').addEventListener('click', deleteFormHandler);
